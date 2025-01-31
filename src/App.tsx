bd0f518ecfed28928eac8eaccd29035a8021378c
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-01-31 08:11:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/Button';
+
+export function App() {
+
+  return (
+    <>
+    <Button variant="primary" />
+    <Button variant="secondary" />
+    <Button variant="success" />
+    <Button variant="danger" />
+    <Button />
+  </>
+  )
+}
+
