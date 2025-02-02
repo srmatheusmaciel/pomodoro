7d272614bcfa486c710bcca052871543843550bd
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-02-02 07:45:44 -0300

    add React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from './components/Button';
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary" />
-    <Button variant="secondary" />
-    <Button variant="success" />
-    <Button variant="danger" />
-    <Button />
-    <GlobalStyle />
-  </ThemeProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
