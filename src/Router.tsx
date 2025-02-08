7b2978a6230c5fedeb0bcd632e864c4f90025536
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2025-02-08 10:52:01 -0300

    Criando History page
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
-import { History } from './pages/History'
-import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
 
-
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-       <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
